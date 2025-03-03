7240dd3593923d454ec97fe26a2519d4c886e3d1
Cezar Pedroso <81267812+cezarpedroso@users.noreply.github.com>
2025-03-03 14:41:02 -0600

    Create python-client.py
    Coded in Python

---
+++ python-client.py
@@ -0,0 +1,9 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1", 8080))
+
+data = client.recv(1024)
+print("Received:", data.decode())
+
+client.close()
